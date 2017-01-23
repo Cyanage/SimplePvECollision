9feb0bf9e363a2093867e320e0f88a31d4e91b61
EarthenSky <gabegames27@gmail.com>
2017-01-23 09:58:17 -0800

    just kept working on the code
    lol code.

--- SimplePvECollision/ICollider.cs
+++ SimplePvECollision/ICollider.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace SimplePvECollision
 {
     interface ICollider
     {
+        Rectangle playerRect { get; set; }      //rectagle that is the collider of the object and the position the texture is drawn at.
+        Texture2D playerTexture { get; set; }   //texture that is drawn to the screen.
+        void Draw(SpriteBatch spriteBatch);     //Draw method so that the collider's texture is drawn to the screen.
     }
 }
