4916041cb57d4aad6dcf3257efdc6ade420b2574
EarthenSky <gabegames27@gmail.com>
2017-01-27 11:24:52 -0800

    made movement.
    collision does not work

--- SimplePvECollision/Character.cs
+++ SimplePvECollision/Character.cs
@@ -47,7 +47,27 @@ namespace SimplePvECollision
 
         public void Update()
         {
-            //TODO: add movement logic here, call this in game1.
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                objectVelocity = new Vector2(objectVelocity.X, -8);
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                objectVelocity = new Vector2(-8, objectVelocity.Y);
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                objectVelocity = new Vector2(objectVelocity.X, 8);
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                objectVelocity = new Vector2(8, objectVelocity.Y);
+            }
+            else
+            {
+                objectVelocity = new Vector2(0, 0);
+            }
+            //TODO: add movement logic here.
         }
     }
 }
